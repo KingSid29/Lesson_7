5bdabdec4d69f20a1fbf2164ef62a24499007839
KingSid29 <siddhantmittal103@gmail.com>
2026-09-21 16:55:31 +0100

    first commit

---
+++ activity_1.py
@@ -0,0 +1,9 @@
+medical_cause = input("Do you have a medical cause (Y/N)").strip().upper()
+if medical_cause == 'Y':
+  print("You are allowed")
+else:
+  attendence = int(input("Enter your attendence"))
+  if attendence >= 75:
+    print("You are allowed")
+  else:
+    print("Your are not allowed ")
